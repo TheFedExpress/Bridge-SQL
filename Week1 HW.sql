65fe75dbf8498f0d05ce0cb3d88382eb0d2a5588
TheFedExpress <pgoodridge2007@gmail.com>
2017-07-22 17:06:22 -0400

    HW 1

---
+++ Week1 HW.sql
@@ -0,0 +1,86 @@
+# Q1
+	select max(distance) as max_dist
+from flights
+;
+# Q2
+	select distinct (engines)
+from planes
+where engines is not null
+order by 1
+;
+create table max_seats as
+	select 
+		engines,
+        max(seats) as most_seats
+	from planes
+    group by 1
+;
+	select
+		*
+	from planes p
+    inner join max_seats ms
+		on p.seats = ms.most_seats
+        and p.engines = ms.engines
+order by seats desc
+;
+# Q3
+	select 
+		COUNT(*) as flights
+	from flights
+;
+# Q4
+	select
+		carrier,
+        count(*) as flights
+	from flights
+    group by 1
+;
+# Q5
+	select 
+		carrier,
+        count(*) as flights
+	from flights
+    group by 1
+    order by 2 desc
+;
+# Q6
+	select 
+		carrier,
+        count(*) as flights
+	from flights
+    group by 1
+    order by 2 desc
+    limit 5
+;
+# Q7
+	select 
+		carrier,
+        count(*) as flights
+	from flights
+    where distance >= 1000
+    group by 1
+    order by 2 desc
+    limit 5
+;
+# Q8
+# Which airports had days with at least 20 MPH winds (not gusts) and how many days
+# were there?
+	select
+		origin,
+        count(*) as windy_days
+	from 
+	  (select
+		origin,
+        year,
+        month,
+        day,
+        max(wind_speed) as speed
+	from weather
+    where wind_speed >= 20
+    group by 1,2,3,4
+    ) as spds
+	where 
+        origin is not null	
+    group by 1
+    order by 2 desc
+;
